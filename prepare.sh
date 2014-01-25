eb70125cb5e6e7af7344e4686cd90e7ed2abda2d
Eric Wright <eric@discoposse.com>
2014-01-25 17:02:32 -0500

    Pipe for tee commands fixed

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,10 @@
 rm /etc/hostname
-echo "aio-openstack" tee /etc/hostname
+echo "aio-openstack" | tee /etc/hostname
 
 rm /etc/hosts
 echo "127.0.0.1 localhost
 ${INTERNAL_IP} aio-havana
-" tee /etc/hosts
+" | tee /etc/hosts
 
 rm /etc/network/interfaces
 echo "
@@ -15,15 +15,20 @@ iface lo inet loopback
 # public IP
 auto eth0
 iface eth0 inet static
-	address 192.168.79.50
+	address ${INTERNAL_IP}
 	netmask 255.255.255.0
-	gateway 192.168.79.2
+	gateway ${INTERNAL_GW}
 	dns-nameservers 8.8.8.8
 
 auto eth1
 iface eth1 inet manual
 	up ifconfig $IFACE 0.0.0.0 up
 	up ifconfig $IFACE promisc
-" tee /etc/network/interfaces
+" | tee /etc/network/interfaces
+
+echo "
+export ${INTERNAL_IP}
+export ${INTERNAL_GW}
+" >> /root/.bashrc
 
 shutdown -h now
